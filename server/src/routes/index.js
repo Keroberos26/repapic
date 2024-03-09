6e61c172ddcf2bb24c06ac9fd2c325815dd7b313
tamminh <nguyendactamminh@gmail.com>
2024-03-09 15:24:53 +0700

    crud Blog basic

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,13 @@
 import productRouter from './product.js';
 import authRouter from './auth.js';
 import cateRouter from './category.js';
+import blogRouter from './blog.js';
 
 const routes = (app) => {
   app.use('/api/auth', authRouter);
   app.use('/api/products', productRouter);
   app.use('/api/categories', cateRouter);
+  app.use('/api/blogs', blogRouter);
 };
 
 export default routes;
