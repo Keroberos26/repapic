65bd3ee3255ccccd69918e94dc060d3f286ec901
Syaoran Nguyen <jinnguyen054@gmail.com>
2024-03-23 09:20:36 +0700

    Dialog, Link and Popover

--- client/src/theme/components/index.ts
+++ client/src/theme/components/index.ts
@@ -3,9 +3,15 @@ import { Theme } from '@mui/material/styles';
 import { MuiAlert } from './alert.modifier';
 import { MuiButton } from './button.modifier';
 import { MuiCheckbox } from './checkbox.modifier';
+import { MuiDialog } from './dialog.modifier';
+import { MuiLink } from './link.modifier';
+import { MuiPopover } from './popover.modifier';
 
 export const components: Components<Theme> = {
   MuiAlert,
   MuiButton,
   MuiCheckbox,
+  MuiDialog,
+  MuiLink,
+  MuiPopover,
 };
